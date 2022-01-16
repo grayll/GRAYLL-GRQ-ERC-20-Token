e2aa8336e17118a69d80c552993c18fcc97e1d67
Abdul Rauf <abdulraufmujahid@gmail.com>
2022-01-16 22:37:36 +0500

    add code generated from https://wizard.openzeppelin.com/

---
+++ migrations/2_deploy.js
@@ -0,0 +1,8 @@
+// migrations/3_deploy_upgradeable_box.js
+const { deployProxy } = require('@openzeppelin/truffle-upgrades');
+
+const GRQv1 = artifacts.require('GRAYLLQU2EST');
+
+module.exports = async function (deployer) {
+  await deployProxy(GRQv1, [], { deployer, initializer: 'initializer' });
+};
