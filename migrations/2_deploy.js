39027e077f0e8668bcbbfc8e3b4b7632c3f8fc26
Abdul Rauf <abdulraufmujahid@gmail.com>
2022-01-16 22:43:48 +0500

    fix: deploy.js

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -4,5 +4,5 @@ const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 const GRQv1 = artifacts.require('GRAYLLQU2EST');
 
 module.exports = async function (deployer) {
-  await deployProxy(GRQv1, [], { deployer, initializer: 'initializer' });
+  await deployProxy(GRQv1, [], { deployer, initializer: 'initialize' });
 };
